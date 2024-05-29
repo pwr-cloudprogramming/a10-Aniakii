355c0d5f8ae87894720d35f4f908962bd7c06478
Aniakii <95714597+Aniakii@users.noreply.github.com>
2024-05-29 20:25:18 +0200

    another solution for env var

--- backend/src/frontend/index.js
+++ backend/src/frontend/index.js
@@ -1,8 +1,8 @@
 const ws = new WebSocket('ws://<EC2_IP>:8080');
 // const ws = new WebSocket('ws://localhost:8080');
 
-console.log("COGNITO_USER_POOL_ID:", process.env.COGNITO_USER_POOL_ID);
-console.log("COGNITO_CLIENT_ID:", process.env.COGNITO_CLIENT_ID);
+console.log("COGNITO_USER_POOL_ID:", "<COGNITO_USER_POOL_ID>");
+console.log("COGNITO_CLIENT_ID:", "<COGNITO_CLIENT_ID>");
 
 function goToLogIn() {
     window.location.href = 'login.html';
