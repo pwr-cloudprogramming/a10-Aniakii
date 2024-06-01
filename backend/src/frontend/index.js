b1f5fb15d9f1333f445aa79700982ac3b9fa6971
Aniakii <95714597+Aniakii@users.noreply.github.com>
2024-06-01 17:29:25 +0200

    added log out funtionality and modified cognito.js

--- backend/src/frontend/index.js
+++ backend/src/frontend/index.js
@@ -1,5 +1,6 @@
+import { signOutUser } from './cognito.js';
+
 const ws = new WebSocket("ws://" + BACKEND_URL + ":8080");
-// const ws = new WebSocket('ws://localhost:8080');
 
 function goToLogIn() {
   window.location.href = "login.html";
@@ -9,3 +10,16 @@ function goToRegister() {
   window.location.href = "register.html";
 }
 
+function logOut() {
+  signOutUser((err, result) => {
+    if (err) {
+      alert(err.message || JSON.stringify(err));
+    } else {
+      alert('Log out successful!');
+    }
+  });
+}
+
+window.goToLogIn = goToLogIn;
+window.goToRegister = goToRegister;
+window.logOut = logOut;
