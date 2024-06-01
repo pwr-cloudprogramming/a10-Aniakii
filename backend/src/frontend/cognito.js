ea41a76651a2be405cd4d15421f66f91d3b2951f
Aniakii <95714597+Aniakii@users.noreply.github.com>
2024-06-01 18:34:12 +0200

    updated cognito.js

--- backend/src/frontend/cognito.js
+++ backend/src/frontend/cognito.js
@@ -1,9 +1,9 @@
 const userPoolId = COGNITO_USER_POOL_ID;
 const clientId = COGNITO_CLIENT_ID;
-
 console.log("USER POOL ID = " + userPoolId);
 console.log("CLIENT ID = " + clientId);
 console.log("USER = " + cognitoUser);
+
 
 function getPoolData() {
   return {
@@ -54,10 +54,9 @@ function signUpUser(userName, userEmail, userPassword, callback) {
         callback(err, null);
       } else {
         cognitoUser = result.user;
-        localStorage.setItem("loggedInUser", userName);
         callback(null, result);
       }
-    }
+    },
   );
 }
 
@@ -71,13 +70,17 @@ function wrapCallback(callback) {
       callback(err, null);
     },
     onSuccess: (result) => {
-      localStorage.setItem("loggedInUser", result.idToken.payload["cognito:username"]);
       callback(null, result);
     },
   };
 }
 
 function signInUser(userName, password, callback) {
+  const loggedInUserName = localStorage.getItem("loggedInUser");
+  if (loggedInUserName) {
+    callback({ name: "Error", message: "Someone is already signed in" }, null);
+    return;
+  }
   let authenticationData = {
     Username: userName,
     Password: password,
@@ -92,15 +95,20 @@ function signInUser(userName, password, callback) {
 }
 
 function signOutUser(callback) {
+
   const loggedInUserName = localStorage.getItem("loggedInUser");
   if (loggedInUserName) {
     cognitoUser = getUser(loggedInUserName);
-    cognitoUser.signOut();
-    localStorage.removeItem("loggedInUser");
-    callback(null, {});
-  } else {
-    callback({ name: "Error", message: "User is not signed in" }, null);
   }
+  if (cognitoUser) {
+    if (cognitoUser.signInUserSession) {
+      cognitoUser.signOut();
+      localStorage.removeItem("loggedInUser");
+      callback(null, {});
+      return;
+    }
+  }
+  callback({ name: "Error", message: "User is not signed in" }, null);
 }
 
 
@@ -109,116 +117,5 @@ export {
   signInUser,
   signOutUser,
   confirmUser,
+  deleteUser,
 };
-
-
-
-// const userPoolId = COGNITO_USER_POOL_ID;
-// const clientId = COGNITO_CLIENT_ID;
-// console.log("USER POOL ID = " + userPoolId);
-// console.log("CLIENT ID = " + clientId);
-
-// function getPoolData() {
-//   return {
-//     UserPoolId: userPoolId,
-//     ClientId: clientId,
-//   };
-// }
-
-// var userPool;
-// var cognitoUser;
-
-// function getUserPool() {
-//   if (!userPool) {
-//     userPool = new AmazonCognitoIdentity.CognitoUserPool(getPoolData());
-//   }
-//   return userPool;
-// }
-
-// function getUser(userName) {
-//   var userData = {
-//     Username: userName,
-//     Pool: getUserPool(),
-//   };
-//   return new AmazonCognitoIdentity.CognitoUser(userData);
-// }
-
-// function signUpUser(userName, userEmail, userPassword, callback) {
-//   let dataEmail = {
-//     Name: "email",
-//     Value: userEmail,
-//   };
-//   let dataName = {
-//     Name: "preferred_username",
-//     Value: userName,
-//   };
-//   let attributeList = [
-//     new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail),
-//     new AmazonCognitoIdentity.CognitoUserAttribute(dataName),
-//   ];
-
-//   getUserPool().signUp(
-//     userName,
-//     userPassword,
-//     attributeList,
-//     null,
-//     function (err, result) {
-//       if (err) {
-//         callback(err, null);
-//       } else {
-//         cognitoUser = result.user;
-//         callback(null, result);
-//       }
-//     },
-//   );
-// }
-
-// function confirmUser(userName, code, callback) {
-//   getUser(userName).confirmRegistration(code, true, callback);
-// }
-
-// function wrapCallback(callback) {
-//   return {
-//     onFailure: (err) => {
-//       callback(err, null);
-//     },
-//     onSuccess: (result) => {
-//       callback(null, result);
-//     },
-//   };
-// }
-
-// function signInUser(userName, password, callback) {
-//   let authenticationData = {
-//     Username: userName,
-//     Password: password,
-//   };
-//   var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
-//     authenticationData,
-//   );
-//   getUser(userName).authenticateUser(
-//     authenticationDetails,
-//     wrapCallback(callback),
-//   );
-// }
-
-// function signOutUser(callback) {
-//   if (cognitoUser) {
-//     if (cognitoUser.signInUserSession) {
-//       cognitoUser.signOut();
-//       localStorage.removeItem("loggedInUser");
-//       callback(null, {});
-//       return;
-//     }
-//   }
-//   callback({ name: "Error", message: "User is not signed in" }, null);
-// }
-
-
-// export {
-//   signUpUser,
-//   signInUser,
-//   signOutUser,
-//   confirmUser,
-//   deleteUser,
-// };
